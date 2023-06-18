c4a35055f73d1223710d9afc4daebb7dfef20662
moulindavid <moulin.david@live.fr>
2023-06-18 19:53:46 +0200

    feat: other find minimum rotated

--- binary_search/find_min_153/src/main.rs
+++ binary_search/find_min_153/src/main.rs
@@ -28,10 +28,35 @@ impl Solution {
         }
         return -1;
     }
+    pub fn find_min_other(nums: Vec<i32>) -> i32 {
+        let mut result = nums[0];
+        let mut lower_bound: i32 = 0;
+        let mut upper_bound: i32 = nums.len() as i32 - 1;
+
+        while lower_bound <= upper_bound {
+            let left = nums[lower_bound as usize];
+            let right = nums[upper_bound as usize];
+            if left < right {
+                //already sorted.
+                result = result.min(left);
+                break;
+            }
+            let middle = (upper_bound + lower_bound) / 2;
+            let middle_value = nums[middle as usize];
+            result = result.min(middle_value);
+
+            if middle_value >= left {
+                lower_bound = middle + 1;
+            } else {
+                upper_bound = middle - 1;
+            }
+        }
+        return result;
+    }
 }
 
 fn main() {
     dbg!(Solution::find_min(vec![3, 4, 5, 1, 2]));
-    dbg!(Solution::find_min(vec![4, 5, 6, 7, 0, 1, 2]));
+    dbg!(Solution::find_min_other(vec![4, 5, 6, 7, 0, 1, 2]));
     dbg!(Solution::find_min(vec![5, 1, 2, 3, 4]));
 }

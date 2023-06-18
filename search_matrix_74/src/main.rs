2e074fd6ae7848dc954060fd8eb125501e86afc8
moulindavid <moulin.david@live.fr>
2023-06-18 10:06:41 +0200

    feat: add some binary search for the matrix

--- search_matrix_74/src/main.rs
+++ search_matrix_74/src/main.rs
@@ -27,6 +27,49 @@ impl Solution {
         }
         return false;
     }
+    pub fn search_matrix_binary_search(nums: Vec<Vec<i32>>, target: i32) -> bool {
+        let rows: i32 = nums.len() as i32;
+        let cols: i32 = nums[0].len() as i32;
+
+        let mut top: i32 = 0;
+        let mut bot: i32 = rows - 1;
+        let mut row = 0;
+        while top <= bot {
+            row = (top + bot) / 2;
+            if *nums[row as usize].last().unwrap() < target {
+                top = row + 1;
+                continue;
+            }
+            if nums[row as usize][0] > target {
+                bot = row - 1;
+            } else {
+                break;
+            }
+        }
+        if top > bot {
+            return false;
+        };
+        let mut lower_bound: i32 = 0;
+        let mut upper_bound: i32 = cols - 1;
+
+        while lower_bound <= upper_bound {
+            let col = (upper_bound + lower_bound) / 2;
+            let middle_value = nums[row as usize][col as usize];
+
+            if middle_value == target {
+                return true;
+            }
+            if middle_value < target {
+                lower_bound = col + 1;
+                continue;
+            }
+
+            if middle_value > target {
+                upper_bound = col - 1;
+            }
+        }
+        return false;
+    }
 }
 
 fn main() {
@@ -38,6 +81,14 @@ fn main() {
         vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]],
         13
     ));
-
+    dbg!(Solution::search_matrix_binary_search(vec![vec![1]], 0));
+    dbg!(Solution::search_matrix_binary_search(
+        vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]],
+        3
+    ));
+    dbg!(Solution::search_matrix_binary_search(
+        vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]],
+        13
+    ));
     dbg!(Solution::search_matrix(vec![vec![1]], 0));
 }
